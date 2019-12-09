85269acfe90ace883655a75988a78e276ef936e0
Simranbal04 <simranbal01@gmail.com>
2019-12-08 22:19:16 -0500

    Main Page Layout

--- app/src/main/java/com/example/teacherinterfaceapp/MainActivity.java
+++ app/src/main/java/com/example/teacherinterfaceapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.teacherinterfaceapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(2000);
+                    Intent intent = new Intent(getApplicationContext(),TeacherLogin.class);
+                    startActivity(intent);
+                    finish();
+                }
+                catch (InterruptedException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 }
