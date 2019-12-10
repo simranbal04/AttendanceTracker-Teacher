5bb21530144f5829706e2ac6307c89507ae88c53
Simranbal04 <simranbal01@gmail.com>
2019-12-09 21:10:44 -0500

    Weekly Attendance Code

--- app/src/main/java/com/example/teacherinterfaceapp/WeeklyAttendance.java
+++ app/src/main/java/com/example/teacherinterfaceapp/WeeklyAttendance.java
@@ -1,8 +1,97 @@
 package com.example.teacherinterfaceapp;
 
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class WeeklyAttendance extends AppCompatActivity {
 
+
+    EditText startdate_et;
+    EditText enddate_et;
+
+    Button mailbutton;
+    LinearLayout classList;
+
+    // Database
+    DatabaseReference attendanceReference; // will fetch daily attendance
+    DatabaseReference studentReference;  // will fetch student data
+
+    //Calendar
+
+    Calendar myCalendar;
+    Calendar startDate;
+    Calendar endDate;
+
+    //datecheck type
+    int start_or_end;
+
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd", Locale.CANADA);
+
+    String date;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weeklyattendance);
+
+
+        startdate_et = (EditText) findViewById(R.id.startdate_et);
+        enddate_et = (EditText) findViewById(R.id.enddate_et);
+        mailbutton = (Button) findViewById(R.id.mailbutton);
+        classList = (LinearLayout) findViewById(R.id.classlist);
+
+        myCalendar = (Calendar) Calendar.getInstance();
+        startDate = (Calendar) Calendar.getInstance();
+        endDate = (Calendar) Calendar.getInstance();
+
+
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        attendanceReference = firebaseDatabase.getReference("dailyattendance");
+        studentReference = firebaseDatabase.getReference("students");
+
+
+        startdate_et.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus)
+                {
+                    start_or_end = 1;
+//                    DatePickerDialog dialog = new DatePickerDialog(WeeklyAttendance.this, date, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH));
+//                    dialog.show();
+
+                }
+                else
+                {
+
+                }
+            }
+        });
+
+//        DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+//            @Override
+//            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth)
+//            {
+//             myCalendar.set(Calendar.YEAR, year);
+//             myCalendar.set(Calendar.MONTH, month);
+//             myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+//
+//            }
+//        }
+
+    }
 
 }
