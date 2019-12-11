ce24e67ea37594f6a6d96ff1138641be476c7b1f
hemantpatel564 <54856451+hemantpatel564@users.noreply.github.com>
2019-12-10 16:44:45 -0800

    refactor 2 today attendence

--- app/src/main/java/com/example/teacherinterfaceapp/Students.java
+++ app/src/main/java/com/example/teacherinterfaceapp/Students.java
@@ -1,25 +1,22 @@
 package com.example.teacherinterfaceapp;
 
 public class Students {
-    private int studentid;
-    private String name,program,classname,instructor,username,password;
 
-    public Students(int studentid, String name, String program, String classname, String instructor, String username, String password) {
-        this.studentid = studentid;
+
+    private String name, program, classname, instructor, studentid, username, password;
+
+    public Students() {
+    }
+
+
+    public Students(String name, String program, String classname, String instructor, String studentid, String username, String password) {
         this.name = name;
         this.program = program;
         this.classname = classname;
         this.instructor = instructor;
+        this.studentid = studentid;
         this.username = username;
         this.password = password;
-    }
-
-    public int getStudentid() {
-        return studentid;
-    }
-
-    public void setStudentid(int studentid) {
-        this.studentid = studentid;
     }
 
     public String getName() {
@@ -54,6 +51,14 @@ public class Students {
         this.instructor = instructor;
     }
 
+    public String getStudentid() {
+        return studentid;
+    }
+
+    public void setStudentid(String studentid) {
+        this.studentid = studentid;
+    }
+
     public String getUsername() {
         return username;
     }
@@ -70,3 +75,82 @@ public class Students {
         this.password = password;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    public Students(int studentid, String name, String program, String classname, String instructor, String username, String password) {
+//        this.studentid = studentid;
+//        this.name = name;
+//        this.program = program;
+//        this.classname = classname;
+//        this.instructor = instructor;
+//        this.username = username;
+//        this.password = password;
+//    }
+//
+//    public int getStudentid() {
+//        return studentid;
+//    }
+//
+//    public void setStudentid(int studentid) {
+//        this.studentid = studentid;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getProgram() {
+//        return program;
+//    }
+//
+//    public void setProgram(String program) {
+//        this.program = program;
+//    }
+//
+//    public String getClassname() {
+//        return classname;
+//    }
+//
+//    public void setClassname(String classname) {
+//        this.classname = classname;
+//    }
+//
+//    public String getInstructor() {
+//        return instructor;
+//    }
+//
+//    public void setInstructor(String instructor) {
+//        this.instructor = instructor;
+//    }
+//
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//}
