18ceaa4fc7891706b5487a86d1a650f265bb772a
Simranbal04 <simranbal01@gmail.com>
2019-12-11 01:15:20 -0500

    Condition for Weekly Attendance

--- app/src/main/java/com/example/teacherinterfaceapp/Students.java
+++ app/src/main/java/com/example/teacherinterfaceapp/Students.java
@@ -4,6 +4,7 @@ public class Students {
 //    private int studentid;
     private String name,program,classname,instructor,username,password,email,studentid;
 
+    public Students(){}
     public Students(String studentid, String name, String program, String classname, String instructor, String username, String password, String email) {
         this.studentid = studentid;
         this.name = name;
@@ -13,22 +14,6 @@ public class Students {
         this.username = username;
         this.password = password;
         this.email = email;
-    }
-
-//    public int getStudentid() {
-//        return studentid;
-//    }
-//
-//    public void setStudentid(int studentid) {
-//        this.studentid = studentid;
-//    }
-
-    public String getStudentid() {
-        return studentid;
-    }
-
-    public void setStudentid(String studentid) {
-        this.studentid = studentid;
     }
 
     public String getName() {
@@ -86,4 +71,83 @@ public class Students {
     public void setEmail(String email) {
         this.email = email;
     }
+
+    public String getStudentid() {
+        return studentid;
+    }
+
+    public void setStudentid(String studentid) {
+        this.studentid = studentid;
+    }
+    //    public int getStudentid() {
+//        return studentid;
+//    }
+//
+//    public void setStudentid(int studentid) {
+//        this.studentid = studentid;
+//    }
+
+//    public Integer getStudentid() {
+//        return studentid;
+//    }
+//
+//    public void setStudentid(String studentid) {
+//        this.studentid = studentid;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getProgram() {
+//        return program;
+//    }
+//
+//    public void setProgram(String program) {
+//        this.program = program;
+//    }
+//
+//    public String getClassname() {
+//        return classname;
+//    }
+//
+//    public void setClassname(String classname) {
+//        this.classname = classname;
+//    }
+//
+//    public String getInstructor() {
+//        return instructor;
+//    }
+//
+//    public void setInstructor(String instructor) {
+//        this.instructor = instructor;
+//    }
+//
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
 }
--- app/src/main/java/com/example/teacherinterfaceapp/WeeklyAttendance.java
+++ app/src/main/java/com/example/teacherinterfaceapp/WeeklyAttendance.java
@@ -1,21 +1,34 @@
 package com.example.teacherinterfaceapp;
 
 import android.app.DatePickerDialog;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Iterator;
 import java.util.Locale;
+import java.util.Set;
+import java.util.TreeMap;
+
+import com.example.teacherinterfaceapp.Students;
 
 public class WeeklyAttendance extends AppCompatActivity {
 
@@ -35,6 +48,7 @@ public class WeeklyAttendance extends AppCompatActivity {
     Calendar myCalendar;
     Calendar startDate;
     Calendar endDate;
+    Calendar startDateC;
 
     //datecheck type
     int start_or_end;
@@ -43,10 +57,13 @@ public class WeeklyAttendance extends AppCompatActivity {
 
     String date;
 
+    TreeMap<Integer,Integer> list;
+    TreeMap<Integer,Students> namelist;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.weeklyattendance);
+
 
 
         startdate_et = (EditText) findViewById(R.id.startdate_et);
@@ -63,6 +80,8 @@ public class WeeklyAttendance extends AppCompatActivity {
         attendanceReference = firebaseDatabase.getReference("dailyattendance");
         studentReference = firebaseDatabase.getReference("students");
 
+        list = new TreeMap<>();
+        namelist = new TreeMap<>();
 
         //    DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener()
         final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener()
@@ -80,7 +99,8 @@ public class WeeklyAttendance extends AppCompatActivity {
                     startDate.set(Calendar.MONTH, month);
                     startDate.set(Calendar.DAY_OF_MONTH, dayOfMonth);
                     startdate_et.setText(sdf.format(myCalendar.getTime()));
-                } else
+                }
+                else
                 {
                     endDate.set(Calendar.YEAR, year);
                     endDate.set(Calendar.MONTH, month);
@@ -92,39 +112,184 @@ public class WeeklyAttendance extends AppCompatActivity {
         };
 
 
-        startdate_et.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+        startdate_et.setOnFocusChangeListener(new View.OnFocusChangeListener()
+        {
             @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if (hasFocus) {
+            public void onFocusChange(View v, boolean hasFocus)
+            {
+                if (hasFocus)
+                {
                     start_or_end = 1;
                     DatePickerDialog dialog = new DatePickerDialog(WeeklyAttendance.this ,date, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH));
                     dialog.show();
 
-                } else {
+                } else
+                    {
 
                 }
             }
         });
 
-        enddate_et.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+        enddate_et.setOnFocusChangeListener(new View.OnFocusChangeListener()
+        {
             @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if (hasFocus) {
+            public void onFocusChange(View v, boolean hasFocus)
+            {
+                if (hasFocus)
+                {
                     start_or_end = 2;
                     DatePickerDialog dialog = new DatePickerDialog(WeeklyAttendance.this, date, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH));
                     dialog.show();
-                } else {
+                } else
+                    {
                 }
 
             }
         });
+        getStudentData();
 
     }
 
     public void chechrecord(View view)
     {
-        
+        String sd = startdate_et.getText().toString();
+        String ed = enddate_et.getText().toString();
+        if (sd.equals("") || ed.equals(""))
+        {
+            Toast.makeText(getApplicationContext(),"Please choose Start Date and End Date", Toast.LENGTH_LONG).show();
+
+        }
+        else
+        {
+            Calendar cal = startDate;
+            cal.add(Calendar.DATE,1);
+            String  date = sdf.format(cal.getTime());
+
+//            Log.d("MSGMSG",startdate_et.getText().toString()+"--"+enddate_et.getText().toString());
+            if (date.equals(enddate_et.getText().toString()))
+            {
+                Log.d("MSGMSG",startdate_et.getText().toString()+"--"+enddate_et.getText().toString());
+
+                int i = 0;
+                startDateC=startDate;
+
+                attendanceReference.orderByKey().startAt(startdate_et.getText().toString()).endAt(enddate_et.getText().toString()).addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshotouter)
+                    {
+                        for (DataSnapshot dataSnapshot : dataSnapshotouter.getChildren())
+                        {
+                            Log.d("MSGMSG", dataSnapshot.getValue()+"");
+                            Set set = list.keySet(); // saving the key value of student
+                            final Iterator iterator = set.iterator();
+                            for (DataSnapshot d:dataSnapshot.getChildren())
+                            {
+                                Log.d("MSGMSG1", dataSnapshot.getChildren()+"");
+
+                                if (dataSnapshot.exists())
+                                {
+                                    while (iterator.hasNext())
+                                    {
+                                        int key = (int) iterator.next();
+                                        if (dataSnapshot.hasChild(key+""))
+                                        {
+                                            int value = list.get(key);
+                                            value=value+1;
+                                            list.put(key,value);
+                                        }
+                                    }
+                                }
+                            }
+
+                        }
+
+                        showData();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
+
+            }
+            else
+                {
+                Toast.makeText(getApplicationContext(),"Please Select Seven Days Difference",Toast.LENGTH_LONG).show();
+
+            }
+        }
     }
+
+
+
+    public void getStudentData()
+    {
+        studentReference.addListenerForSingleValueEvent(new ValueEventListener()
+        {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot)
+            {
+                if (dataSnapshot.exists())
+                {
+                    for (DataSnapshot d : dataSnapshot.getChildren())
+                    {
+                        Students obj = d.getValue(Students.class);
+                        list.put(Integer.valueOf(obj.getStudentid()),0);
+                        namelist.put(Integer.valueOf(obj.getStudentid()),obj);
+
+
+
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+    public void showData()
+    {
+//        classList.removeAllViews();
+        Set set = list.keySet();
+        Iterator iterator  = set.iterator();
+        int i = 0;
+        while (iterator.hasNext())
+        {
+            LinearLayout layout2 = new LinearLayout(getApplicationContext());
+            layout2.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.WRAP_CONTENT));
+            layout2.setOrientation(LinearLayout.HORIZONTAL);
+            layout2.setWeightSum(10);
+
+            int key = (int) iterator.next();
+
+            TextView textView = new TextView(this);
+            textView.setLayoutParams(new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.WRAP_CONTENT,8));
+            textView.setText(namelist.get(key).getName());
+            textView.setTextColor(Color.parseColor("#000000"));
+            textView.setTextSize(22);
+
+            layout2.addView(textView);
+
+            TextView textView1 = new TextView(this);
+            textView1.setLayoutParams(new LinearLayout.LayoutParams(0,LinearLayout.LayoutParams.WRAP_CONTENT,2));
+            textView1.setText(((int)((float)list.get(key)/2*100))+"%");
+            textView1.setTextColor(Color.parseColor("#000000"));
+            textView1.setTextSize(22);
+
+            if (((int)((float)list.get(key)/2*100))<50)
+            {
+                layout2.addView(textView1);
+                classList.addView(layout2);
+            }
+
+        }
+    }
+
 
 
 
